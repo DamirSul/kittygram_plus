63abcd0fbbac2fc66927aa664e849e5b768565fd
DamirSul <_@example.com>
2024-07-02 17:14:22 +0600

    comp comit

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -13,5 +13,11 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    #path('api-token-auth/', views.obtain_auth_token),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    # auth/users/ - new user
+    # auth/users/me/ - get or put new user
+    # auth/jwt/create/ - create jwt
+    # auth/jwt/refresh/ - get new jwt
 ]
